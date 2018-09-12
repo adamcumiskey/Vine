fde984fedf671cadd4fac5252fa0c2caf4dd6d1d
Adam <adam.cumiskey@gmail.com>
2018-09-11 20:39:06 -0400

    Adding convenience methods

---
+++ UIViewController+Vine.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Vine
+//
+//  Created by Adam on 9/10/18.
+//
+
+import UIKit
+
+extension Vine where Root == UIViewController {
+    func dismiss(animated: Bool, completion: (() -> Void)? = nil) {
+        root?.dismiss(animated: animated, completion: completion)
+    }
+    
+    func show(_ viewController: UIViewController, sender: Any? = nil) {
+        root?.show(viewController, sender: sender)
+    }
+    
+    func showDetail(_ viewController: UIViewController, sender: Any? = nil) {
+        root?.showDetailViewController(viewController, sender: sender)
+    }
+}
---
+++ Window.swift
@@ -0,0 +1,30 @@
+//
+//  Window.swift
+//  Vine
+//
+//  Created by Adam on 9/10/18.
+//
+
+import UIKit
+
+public class Window: UIWindow {
+    /// Reference to the Vine
+    var vine: Vine<Window>
+    
+    public init(frame: CGRect, vine: Vine<Window>) {
+        self.vine = vine
+        super.init(frame: frame)
+        self.vine.root = self
+        self.vine.start()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension Vine where Root == Window {
+    func setRootViewController(_ viewController: UIViewController) {
+        root?.rootViewController = viewController
+    }
+}
